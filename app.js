6b66d2c3745686cdec65d9aa38c978717a2e13b3
tabassum2507 <10tabassum2@gmail.com>
2021-12-19 18:53:38 +0530

    finished

---
+++ app.js
@@ -0,0 +1,19 @@
+const circle = document.querySelector(".progress-circle");
+const loading = document.querySelector(".loading");
+
+const fakeUploadPercentage = [0, 10, 25, 40, 42, 60, 70, 75, 90, 100];
+let i = 0;
+
+const circumference = circle.getTotalLength();
+
+const interval = setInterval(() => {
+  circle.style.strokeDashoffset =
+    circumference - (fakeUploadPercentage[i] / 100) * circumference;
+  loading.innerHTML = fakeUploadPercentage[i] + "%";
+  i++;
+  console.log(i);
+  if (i == fakeUploadPercentage.length) {
+    clearInterval(interval);
+    loading.innerHTML = "Ok :)";
+  }
+}, 1000);
